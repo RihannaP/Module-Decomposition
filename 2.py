24cdc6496860c669e38c0cd51d9289da9bde8232
Rihanna P <114884466+RihannaP@users.noreply.github.com>
2025-10-26 00:16:17 +0100

    Implemment Ex2

--- 2.py
+++ 2.py
@@ -1,14 +1,25 @@
-def open_account(balances, name, amount):
+from typing import List, Dict, Union
+
+def open_account(balances: dict[str, int], name: str, amount: Union[int, float, str]) -> None :
+    if isinstance(amount, str) and amount.startswith("£"):
+        pounds, pence = map(int, amount[1:].split("."))
+        amount = pounds * 100 + pence
+    elif isinstance(amount, float):
+        amount = int(round(amount * 100))
+    elif isinstance(amount, int):
+        pass
+    else:
+        raise ValueError("Unsupported amount format")
     balances[name] = amount
 
-def sum_balances(accounts):
+def sum_balances(accounts: Dict[str, int]) -> int:
     total = 0
     for name, pence in accounts.items():
         print(f"{name} had balance {pence}")
         total += pence
     return total
 
-def format_pence_as_string(total_pence):
+def format_pence_as_str(total_pence: int) -> str:
     if total_pence < 100:
         return f"{total_pence}p"
     pounds = int(total_pence / 100)
@@ -21,10 +32,10 @@ balances = {
     "Georg": 831,
 }
 
-open_account("Tobi", 9.13)
-open_account("Olya", "£7.13")
+open_account(balances,"Tobi", 9.13)
+open_account(balances,"Olya", "£7.13")
 
-total_pence = sum_balances(balances)
-total_string = format_pence_as_str(total_pence)
+total_pence:int = sum_balances(balances)
+total_string: str = format_pence_as_str(total_pence)
 
 print(f"The bank accounts total {total_string}")
